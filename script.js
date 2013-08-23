d7105ed119e02562830a75a700f5ba025a200fe2
Conrad Zimmerman <me@conradz.com>
2013-08-23 17:21:25 +0000

    Update example

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 ;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var tabs = require('../'),
-    events = require('chi-events');
+    events = require('chi-events'),
+    document = window.document;
 
 var myTabs = tabs(document.querySelector('#my-tabs'));
 
@@ -11,6 +12,7 @@ events(document.querySelector('#select-first')).on('click', function() {
 events(document.querySelector('#select-dogs')).on('click', function() {
     myTabs.select(document.querySelector('#dogs'));
 });
+
 },{"../":2,"chi-events":4}],2:[function(require,module,exports){
 var EventEmitter = require('events').EventEmitter,
     _ = require('lodash'),
